5b1737f0ef28a76f5981b9993c864b06f9789880
waytosucceed <waytosucceedbyjaspal@gmail.com>
2024-09-03 09:10:20 +0530

    first commit

--- questions.js
+++ questions.js
@@ -7,7 +7,7 @@ var previousQuestionIndex = null; // Track the previously displayed question
 var topicName = ''; // Variable to store the topic name
 const submitSound =document.getElementById("submit-sound");
 
-const uniqueKey = 2_15;
+const uniqueKey = 2_16;
 
 // Helper function to save data in local storage under the unique key
 function saveToLocalStorage(key, value) {
